c4e2d8c7b512a6192429696b91ac5633b4e94b57
jacobntran <jacobntran@gmail.com>
2024-02-23 11:33:15 -0800

    converted subnets into a resuable tf module

---
+++ modules/networking/subnet/vars.tf
@@ -0,0 +1,15 @@
+variable "vpc_id" {
+    description = "ID of the VPC for the subnet"
+}
+
+variable "cidr_block" {
+    description = "CIDR block for the subnet"
+}
+
+variable "availability_zone" {
+    description = "AZ to place the subnet in"
+}
+
+variable "subnet_name" {
+    description = "Name of the subnet"
+}
--- networking/main.tf
+++ networking/main.tf
@@ -10,122 +10,110 @@ resource "aws_vpc" "this" {
   }
 }
 
-resource "aws_subnet" "public_1" {
+module "public_subnet_az1" {
+  source = "../modules/networking/subnet"
+
   vpc_id = aws_vpc.this.id
   cidr_block = "10.0.0.0/20"
   availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "Public-1"
-  }
+  subnet_name = "Public Subnet AZ1"
 }
 
-resource "aws_subnet" "private_1" {
+module "private_compute_subnet_az1" {
+  source = "../modules/networking/subnet"
+
   vpc_id = aws_vpc.this.id
   cidr_block = "10.0.16.0/20"
   availability_zone = "us-east-1a"
+  subnet_name = "Private Compute Subnet AZ1"
+}
 
-  tags = {
-    Name = "Private-1"
-  }
-} 
+module "private_data_subnet_az1" {
+  source = "../modules/networking/subnet"
 
-resource "aws_subnet" "data_1" {
   vpc_id = aws_vpc.this.id
   cidr_block = "10.0.32.0/20"
   availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "Data-1"
-  }
+  subnet_name = "Private Data Subnet AZ1"
 }
 
-resource "aws_subnet" "reserved_1" {
+module "reserved_subnet_az1" {
+  source = "../modules/networking/subnet"
+
   vpc_id = aws_vpc.this.id
   cidr_block = "10.0.48.0/20"
   availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "Reserved-1"
-  }
+  subnet_name = "Reserved Subnet AZ1"
 }
 
-resource "aws_subnet" "public_2" {
+module "public_subnet_az2" {
+  source = "../modules/networking/subnet"
+
   vpc_id = aws_vpc.this.id
   cidr_block = "10.0.64.0/20"
   availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "Public-2"
-  }
+  subnet_name = "Public Subnet AZ2"
 }
 
-resource "aws_subnet" "private_2" {
+module "private_compute_subnet_az2" {
+  source = "../modules/networking/subnet"
+
   vpc_id = aws_vpc.this.id
   cidr_block = "10.0.80.0/20"
   availability_zone = "us-east-1b"
+  subnet_name = "Private Compute Subnet AZ2"
+}
 
-  tags = {
-    Name = "Private-2"
-  }
-} 
+module "private_data_subnet_az2" {
+  source = "../modules/networking/subnet"
 
-resource "aws_subnet" "data_2" {
   vpc_id = aws_vpc.this.id
   cidr_block = "10.0.96.0/20"
   availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "Data-2"
-  }
+  subnet_name = "Private Data Subnet AZ2"
 }
 
-resource "aws_subnet" "reserved_2" {
+module "reserved_subnet_az2" {
+  source = "../modules/networking/subnet"
+
   vpc_id = aws_vpc.this.id
   cidr_block = "10.0.112.0/20"
   availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "Reserved-2"
-  }
+  subnet_name = "Reserved Subnet AZ2"
 }
 
-resource "aws_subnet" "public_3" {
+module "public_subnet_az3" {
+  source = "../modules/networking/subnet"
+
   vpc_id = aws_vpc.this.id
   cidr_block = "10.0.128.0/20"
   availability_zone = "us-east-1c"
-
-  tags = {
-    Name = "Public-3"
-  }
+  subnet_name = "Public Subnet AZ3"
 }
 
-resource "aws_subnet" "private_3" {
+module "private_compute_subnet_az3" {
+  source = "../modules/networking/subnet"
+
   vpc_id = aws_vpc.this.id
   cidr_block = "10.0.144.0/20"
   availability_zone = "us-east-1c"
+  subnet_name = "Private Compute Subnet AZ3"
+}
 
-  tags = {
-    Name = "Private-3"
-  }
-} 
+module "private_data_subnet_az3" {
+  source = "../modules/networking/subnet"
 
-resource "aws_subnet" "data_3" {
   vpc_id = aws_vpc.this.id
   cidr_block = "10.0.160.0/20"
   availability_zone = "us-east-1c"
-
-  tags = {
-    Name = "Data-3"
-  }
+  subnet_name = "Private Data Subnet AZ3"
 }
 
-resource "aws_subnet" "reserved_3" {
+module "reserved_subnet_az3" {
+  source = "../modules/networking/subnet"
+
   vpc_id = aws_vpc.this.id
   cidr_block = "10.0.176.0/20"
   availability_zone = "us-east-1c"
-
-  tags = {
-    Name = "Reserved-3"
-  }
+  subnet_name = "Reserved Subnet AZ3"
 }
